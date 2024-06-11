f73802e60f8c39e67c4305f775accd4771a9190f
chase <chase.seltz21@gmail.com>
2024-06-11 14:41:12 -0700

    Make Guest's Address2 an optional property

--- API/Models/Guest.cs
+++ API/Models/Guest.cs
@@ -9,7 +9,7 @@ public class Guest
     public string? CompanyName { get; set; }
     public string Country { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
-    public string Address2 { get; set; } = string.Empty;
+    public string? Address2 { get; set; }
     public string City { get; set; } = string.Empty;
     public string State { get; set; } = string.Empty;
     public string Zip { get; set; } = string.Empty;
