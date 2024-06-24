21ae1c1ebb52bdb2282daee3d31b3f420f3edf8d
chase <chase.seltz21@gmail.com>
2024-06-24 15:17:17 -0700

    Create AnimatedLink component

---
+++ client/src/components/AnimatedLink/AnimatedLink.tsx
@@ -0,0 +1,19 @@
+import styles from "./AnimatedLink.module.scss";
+import { CSSProperties, ReactNode } from "react";
+
+interface Props {
+    children?: ReactNode;
+    href?: string;
+    className?: string;
+    styles?: CSSProperties;
+}
+
+const Link = (props: Props) => {
+    return (
+        <a className={`${styles.link} ${props.className || ""}`} style={props.styles} href={props.href || "#"}>
+            {props.children}
+        </a>
+    );
+};
+
+export default Link;
