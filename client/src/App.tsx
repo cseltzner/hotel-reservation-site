e815ff56507d97c8d6265824c974552fb5bf69b2
chase <chase.seltz21@gmail.com>
2024-06-24 21:56:53 -0700

    Create HeaderSecondary component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import HeaderSecondary from "./layout/HeaderSecondary/HeaderSecondary.tsx";
 
 const App = () => {
     return (
         <div>
+            <HeaderSecondary />
             <Outlet />
         </div>
     );
---
+++ client/src/layout/HeaderSecondary/HeaderSecondary.tsx
@@ -0,0 +1,20 @@
+import styles from "./HeaderSecondary.module.scss";
+import AnimatedLink from "../../components/AnimatedLink/AnimatedLink.tsx";
+
+const HeaderSecondary = () => {
+    return (
+        <div className={styles.header}>
+            <div className={`container ${styles.headerContainer}`}>
+                <div className={styles.headerLeft}>
+                    <AnimatedLink className={styles.headerItem} href="tel:1562409217">(156)-240-9217</AnimatedLink>
+                    <AnimatedLink className={styles.headerItem} href="mailto:help@hotelseltz.com">help@hotelseltz.com</AnimatedLink>
+                </div>
+                <div className={styles.headerRight}>
+                    <p className={styles.headerItem}>New York <span className={styles.tempDegrees}>80&deg;F</span></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderSecondary;
