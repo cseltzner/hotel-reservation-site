05d6f7ac85d16a4c99f92b3cffc643fcdb3b9b1a
chase <chase.seltz21@gmail.com>
2024-06-30 18:40:26 -0700

    Add fixed Github link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import styles from "./App.module.scss";
 import { Outlet } from "react-router-dom";
 import Header from "./layout/Header/Header.tsx";
 import NavMenu from "./layout/NavigationMenu/NavMenu.tsx";
@@ -5,6 +6,7 @@ import { useEffect, useState } from "react";
 import { RoomName } from "./interfaces/RoomName.ts";
 import { apiUrls } from "./http/urls.ts";
 import Footer from "./layout/Footer/Footer.tsx";
+import Github from "./components/Icons/Github.tsx";
 
 const NUM_ROOM_NAMES_TO_FETCH = 5; // Used to fetch a certain number of room names from API for "Rooms" tab dropdown
 
@@ -21,11 +23,14 @@ const App = () => {
     }, []);
 
     return (
-        <div>
+        <div className={styles.appWrapper}>
             <Header />
             <NavMenu roomNames={roomNames} />
             <Outlet />
             <Footer roomNames={roomNames} />
+            <a href="https://www.github.com/cseltzner" target="_blank">
+                <Github className={styles.github} />
+            </a>
         </div>
     );
 };
