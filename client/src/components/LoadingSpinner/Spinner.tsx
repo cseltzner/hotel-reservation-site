717b09a03f485084e651894bc48cdcedde1ddea8
chase <chase.seltz21@gmail.com>
2024-07-15 20:42:38 -0700

    Add Spinner component

---
+++ client/src/components/LoadingSpinner/Spinner.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Spinner.keyframes.css";
+
+interface Props {
+    size?: number;
+    color?: string;
+    animDurationMs?: number;
+    left?: string;
+    right?: string;
+    top?: string;
+    bottom?: string;
+    transform?: string;
+    position?: "fixed" | "absolute";
+}
+
+const Spinner = ({
+                     size,
+                     color,
+                     animDurationMs,
+                     left,
+                     right,
+                     top,
+                     bottom,
+                     transform,
+                     position,
+                 }: Props) => {
+    const animationDurationMs = animDurationMs || 2000;
+    return (
+        <>
+            <div
+                role={"progressbar"}
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    position: `${position || "absolute"}`,
+                    left: `${left || 0}`,
+                    right: `${right || 0}`,
+                    top: `${top || 0}`,
+                    bottom: `${bottom || 0}`,
+                    transform: `${transform || null}`,
+                }}
+            >
+                <div
+                    style={{
+                        padding: `${size || "12"}px`,
+                        marginLeft: `${size! / 3 || 4}px`,
+                        marginRight: `${size! / 3 || 4}px`,
+                        backgroundColor: color || "#000",
+                        animation: `cs-custom-spinner-pop-in ${
+                            animationDurationMs || 750
+                        }ms ease-in-out infinite`,
+                        animationDelay: `${animationDurationMs * 0}ms`,
+                        borderRadius: "9999px",
+                        display: "inline-block",
+                    }}
+                ></div>
+                <div
+                    style={{
+                        padding: `${size || "12"}px`,
+                        marginLeft: `${size! / 3 || 4}px`,
+                        marginRight: `${size! / 3 || 4}px`,
+                        backgroundColor: color || "#000",
+                        animation: `cs-custom-spinner-pop-in ${
+                            animationDurationMs || 750
+                        }ms ease-in-out infinite`,
+                        animationDelay: `${animationDurationMs * 0.1}ms`,
+                        borderRadius: "9999px",
+                        display: "inline-block",
+                    }}
+                ></div>
+                <div
+                    style={{
+                        padding: `${size || "12"}px`,
+                        marginLeft: `${size! / 3 || 4}px`,
+                        marginRight: `${size! / 3 || 4}px`,
+                        backgroundColor: color || "#000",
+                        animation: `cs-custom-spinner-pop-in ${
+                            animationDurationMs || 750
+                        }ms ease-in-out infinite`,
+                        animationDelay: `${animationDurationMs * 0.2}ms`,
+                        borderRadius: "9999px",
+                        display: "inline-block",
+                    }}
+                ></div>
+            </div>
+        </>
+    );
+};
+
+export default Spinner;
