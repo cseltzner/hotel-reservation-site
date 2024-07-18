8f470f8d9d475b2494a25ed67efde3c34f58f52e
chase <chase.seltz21@gmail.com>
2024-07-17 18:44:48 -0700

    Extract Booking to its own component

---
+++ client/src/components/Booking/BookingComponent.tsx
@@ -0,0 +1,67 @@
+import styles from "./BookingComponent.module.scss";
+import { format } from "date-fns";
+import {
+    calculateNumNights,
+    calculateRoomPriceEstimate,
+    calculateTotalPriceEstimate
+} from "../../helpers/bookingHelpers.ts";
+import { Booking } from "../../interfaces/models/Booking.ts";
+
+interface Props {
+    booking: Booking
+}
+
+const currencyFormatter = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+})
+
+const BookingComponent = ({booking}: Props) => {
+    return (
+        <div className={styles.booking}>
+            <div className={styles.bookingDetailsContainer}>
+                <p className={styles.bookingDetail}>Order Number: <span
+                    className={styles.bookingDetailData}>{booking.id}</span></p>
+                <p className={styles.bookingDetail}>Total: <span
+                    className={styles.bookingDetailData}>{currencyFormatter.format(booking.bookingTotal)}</span>
+                </p>
+                <p className={styles.bookingDetail}>Payment Method: <span
+                    className={styles.bookingDetailData}>{booking.paymentMethod.name}</span></p>
+            </div>
+            <h2>Order Details</h2>
+            <ul className={styles.orderList}>
+                {booking.bookingRooms.map(br => (
+                    <li key={br.id} className={styles.orderItem}>
+                        <div className={styles.orderItemDetails}>
+                            <a href={`/rooms/${br.room.id}`}>{br.room.name}</a>
+                            <p>{format(br.checkInDate, "MMMM d")} -
+                                {format(br.checkOutDate, "MMMM d")}
+                                <span
+                                    className={styles.orderItemDays}>({calculateNumNights(br.checkInDate, br.checkOutDate)} day{calculateNumNights(br.checkInDate, br.checkOutDate) > 1 ? "s" : ""})</span>
+                            </p>
+                            <p>{`${br.numGuests} Guest${br.numGuests > 1 ? "s" : ""}`}</p>
+                            <p>
+                                {br.extraServices.map((service, index) => (
+                                    <span key={service.id}
+                                          className={styles.roomServices}>{service.name}{index < br.extraServices.length - 1 ? ", " : "."}</span>
+                                ))}
+                            </p>
+                        </div>
+                        <div
+                            className={styles.orderItemPrice}>{currencyFormatter.format(calculateRoomPriceEstimate(br))}</div>
+                    </li>
+                ))}
+            </ul>
+            <div className={styles.totalsContainer}>
+                <p>Subtotal</p>
+                <p>{currencyFormatter.format(calculateTotalPriceEstimate(booking.bookingRooms))}</p>
+            </div>
+            <div className={styles.totalsContainer}>
+                <p>Total</p>
+                <p className={styles.bold}>{currencyFormatter.format(calculateTotalPriceEstimate(booking.bookingRooms))}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BookingComponent;
--- client/src/pages/CheckoutSuccess/CheckoutSuccessPage.tsx
+++ client/src/pages/CheckoutSuccess/CheckoutSuccessPage.tsx
@@ -8,15 +8,11 @@ import {
     calculateTotalPriceEstimate
 } from "../../helpers/bookingHelpers.ts";
 import { useEffect } from "react";
+import BookingComponent from "../../components/Booking/BookingComponent.tsx";
 
 const CheckoutSuccessPage = () => {
     const navigate = useNavigate();
     const booking: Booking = useLocation().state;
-
-    const currencyFormatter = Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    })
 
     useEffect(() => {
         if (!booking) navigate("/");
@@ -27,47 +23,7 @@ const CheckoutSuccessPage = () => {
             {booking && (
                 <div className="container">
                     <h1>Thank you for your order.</h1>
-                    <div className={styles.bookingDetailsContainer}>
-                        <p className={styles.bookingDetail}>Order Number: <span
-                            className={styles.bookingDetailData}>{booking.id}</span></p>
-                        <p className={styles.bookingDetail}>Total: <span
-                            className={styles.bookingDetailData}>{currencyFormatter.format(booking.bookingTotal)}</span>
-                        </p>
-                        <p className={styles.bookingDetail}>Payment Method: <span
-                            className={styles.bookingDetailData}>{booking.paymentMethod.name}</span></p>
-                    </div>
-                    <h2>Order Details</h2>
-                    <ul className={styles.orderList}>
-                        {booking.bookingRooms.map(br => (
-                            <li key={br.id} className={styles.orderItem}>
-                                <div className={styles.orderItemDetails}>
-                                    <a href={`/rooms/${br.room.id}`}>{br.room.name}</a>
-                                    <p>{format(br.checkInDate, "MMMM d")} -
-                                        {format(br.checkOutDate, "MMMM d")}
-                                        <span
-                                            className={styles.orderItemDays}>({calculateNumNights(br.checkInDate, br.checkOutDate)} day{calculateNumNights(br.checkInDate, br.checkOutDate) > 1 ? "s" : ""})</span>
-                                    </p>
-                                    <p>{`${br.numGuests} Guest${br.numGuests > 1 ? "s" : ""}`}</p>
-                                    <p>
-                                        {br.extraServices.map((service, index) => (
-                                            <span key={service.id}
-                                                  className={styles.roomServices}>{service.name}{index < br.extraServices.length - 1 ? ", " : "."}</span>
-                                        ))}
-                                    </p>
-                                </div>
-                                <div
-                                    className={styles.orderItemPrice}>{currencyFormatter.format(calculateRoomPriceEstimate(br))}</div>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className={styles.totalsContainer}>
-                        <p>Subtotal</p>
-                        <p>{currencyFormatter.format(calculateTotalPriceEstimate(booking.bookingRooms))}</p>
-                    </div>
-                    <div className={styles.totalsContainer}>
-                        <p>Total</p>
-                        <p className={styles.bold}>{currencyFormatter.format(calculateTotalPriceEstimate(booking.bookingRooms))}</p>
-                    </div>
+                    <BookingComponent booking={booking}/>
                     <a href="/" className={styles.returnButton}>Return Home</a>
                 </div>
             )}
